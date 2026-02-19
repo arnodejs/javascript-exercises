7f6c568133031d0879d0b2b8f2c456933722b5df
Arno <arn.hovas@gmail.com>
2026-02-19 23:19:19 +0400

    Complete task reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.length === 0
+    ? ''
+    : string.split('').reduceRight((str, curr) => str + curr);
 };
 
 // Do not edit below this line
