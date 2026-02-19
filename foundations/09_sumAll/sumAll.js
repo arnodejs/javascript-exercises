285f3f7282fa6107a903da70d4a8313c1f73d70c
Arno <arn.hovas@gmail.com>
2026-02-19 23:34:42 +0400

    Complete task sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (
+    typeof int1 !== 'number' ||
+    typeof int2 !== 'number' ||
+    !Number.isInteger(int1) ||
+    !Number.isInteger(int2) ||
+    int1 < 0 ||
+    int2 < 0
+  ) {
+    return 'ERROR';
+  }
 
+  let start, end;
+
+  if (int1 > int2) {
+    start = int2;
+    end = int1;
+  } else if (int1 < int2) {
+    start = int1;
+    end = int2;
+  }
+
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
