55b7ebe3acd92e322ae27816d8db37e3ae108e27
Arno <arn.hovas@gmail.com>
2026-02-21 20:42:15 +0400

    Complete palindromes task

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const punctuations = [',', '.', '!', '?', '-', ' '];
+  let newString = '';
+  let reversedString = '';
 
+  for (let i = 0; i < string.length; i++) {
+    if (punctuations.includes(string[i])) continue;
+    else {
+      newString += string[i].toLowerCase();
+    }
+  }
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    if (punctuations.includes(string[i])) continue;
+    else {
+      reversedString += string[i].toLowerCase();
+    }
+  }
+
+  return reversedString === newString;
 };
+
+console.log(palindromes('A man, a car, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
