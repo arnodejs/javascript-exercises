c37985b5a78e9cf7e5be408fe2dc2d8f8561d030
Arno <arn.hovas@gmail.com>
2026-02-23 14:44:26 +0400

    Complete fibonacci task

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return 'OOPS';
 
+  let numbers = {
+    0: 0,
+    1: 1,
+  };
+
+  for (let i = 2; i <= n; i++) {
+    numbers[i] = numbers[i - 1] + numbers[i - 2];
+  }
+
+  return numbers[n];
 };
 
 // Do not edit below this line
