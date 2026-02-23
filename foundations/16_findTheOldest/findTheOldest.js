3aee9296bf34cb23426faeef84157446d0038992
Arno <arn.hovas@gmail.com>
2026-02-23 14:57:45 +0400

    Complete findTheOldest task

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people
+    .map((person) => {
+      const age = person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth;
+      return {
+        ...person,
+        age,
+      };
+    })
+    .sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
